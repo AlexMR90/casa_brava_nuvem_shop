ae5815fb57aa979a1543647ac486eb8e20c97bf6
TPirate90 Punk Lógico <alex.martins.r.ti@gmail.com>
2024-11-25 23:07:17 -0300

    Update index.js v2

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-/* botão zap
-document.addEventListener("scroll", () => {
-    const zapButton = document.getElementById("zap");
-    const targetSection = document.querySelector("#Categorias"); // Seção onde o botão deve aparecer
-    
-    // Verifica a posição da seção em relação ao scroll
-    const sectionTop = targetSection.offsetTop;
-    const currentScroll = window.scrollY;
-
-    // Exibe o botão se o scroll estiver além do topo da seção
-    if (currentScroll >= sectionTop) {
-        zapButton.style.display = "flex"; // Mostra o botão
-    } else {
-        zapButton.style.display = "none"; // Oculta o botão
-    }
-});*/
-/* Fim botão zap*/
 /*botão topo*/
-document.addEventListener("scroll", () => {
+document.addEventListener("DOMContentLoaded", () => {
     const btntopoButton = document.getElementById("btntopo");
-    const targetSection = document.querySelector("#Categorias"); // Seção onde o botão deve aparecer
-    
-    // Verifica a posição da seção em relação ao scroll
-    const sectionTop = targetSection.offsetTop;
-    const currentScroll = window.scrollY;
+    const targetSections = document.querySelectorAll("#Atendimento,#Mapa, .SN, .Politicas");
 
-    // Exibe o botão se o scroll estiver além do topo da seção
-    if (currentScroll >= sectionTop) {
-        btntopoButton.style.display = "flex"; // Mostra o botão
-    } else {
-        btntopoButton.style.display = "none"; // Oculta o botão
-    }
+    // Configura o IntersectionObserver
+    const observer = new IntersectionObserver((entries) => {
+        let isVisible = false;
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                isVisible = true;
+            }
+        });
+
+
+        // Exibe ou oculta o botão com base na visibilidade das seções
+        if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+            btntopoButton.style.display = "flex";
+        } else {
+        btntopoButton.style.display = isVisible ? "flex" : "none";
+    }}, { threshold: 0.7 }); // Detecta 10% da seção visível
+
+    // Observa cada seção
+    targetSections.forEach(section => {
+        observer.observe(section);
+    });
 });
+
 /*Fim botão topo */
